80765186fce0320c161c3a2b0bd88a88b7560450
aliaasghar <ali_asghar@live.co.uk>
2018-05-20 19:05:54 +0100

    reverse a string

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-def reverse_string(a_string):
-       return a_string[::-1]
-test = reverse_string("tammy")
-print(test)
+# # 1.0 Reverse a string
+# def reverse_string(a_string):
+#        return a_string[::-1]
+# test = reverse_string("tammy")
+# print(test)
+
