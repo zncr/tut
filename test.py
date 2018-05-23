89b7b3860adb3dd4601c70e8c23cb3317df0f914
aliaasghar <ali_asghar@live.co.uk>
2018-05-23 19:53:06 +0100

    flip a matrix

--- test.py
+++ test.py
@@ -17,6 +17,7 @@
 # list = set(mylist)
 # print(list)
 
+
 def flip_vertical_axis(matrix):    
     r = len(matrix) - 1
     c = len(matrix[0]) - 1
@@ -26,12 +27,15 @@ def flip_vertical_axis(matrix):
         j = 0
         while j <= (c/2):
             temp = matrix[i][j]
-            matrix[i][j] = matrix[i][c - j]
-            matrix[i][c - j] = temp
+            matrix[i][j] = matrix[i][c - j]     
+            matrix[i][c - j] = temp        
             j = j + 1
         i = i + 1
+        
     return matrix
 x = [[1,0]]
 s =flip_vertical_axis([[1,2]])
 
-print(s)
+# print(s)
+g =flip_vertical_axis([[2,4,7,9],[5,6,8,10]])
+print(g)
